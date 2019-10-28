454472ffb81583c8e91c12bbea7c248789c65749
Toni Gallardo <agallardol2007@gmail.com>
2019-10-28 13:41:42 +0100

    Upload

---
+++ JuegoDeLaSerpiente/ClassLibrary1/ClassCell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JuegoDeLaSerpiente
+{
+    public class ClassCell
+    {
+        // Atributes
+        int CellState; // 0 for empty cell, 1 for snake head
+
+        //Methods
+        //Constructor
+        public ClassCell()
+            {
+            this.CellState = 0;
+            }
+        //--------------------------------
+        public void SetCellState(int State)
+        {
+            this.CellState = State;
+        }
+
+        public int GetCellState()
+        {
+            return this.CellState;
+        }
+
+    }
+}
---
+++ JuegoDeLaSerpiente/ClassLibrary1/ClassGrid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuegoDeLaSerpiente
+{
+    public class ClassGrid
+    {
+        //Atributes
+        ClassCell[,] Grid;
+
+        //Constructor
+        public ClassGrid()
+        {
+            //Generació de la malla
+            this.Grid = new ClassCell[100,100];
+            for(int j = 0 ; j<100; j++)
+            {
+                for(int i = 0; i<100; i++)
+                {
+                    this.Grid[j, i] = new ClassCell();
+                }
+            }
+            //Generació de les parets
+
+
+        }
+
+        //Methods
+
+    }
+}
